df8db72a1c7b60bbc41ac3f59e1ad183f9e9bdb2
Layna Oyervides <laynaoyervides@gmail.com>
2021-08-14 17:57:01 -0600

    commit tree cbcb716c34c189d24918185ea62751c37b23b68a

--- index.js
+++ index.js
@@ -1,28 +1,22 @@
 // Code your solution in this file!
-let drivers = [Charles, Erick, Mirna, Carlos, Luis, Mario];
+let drivers = ['Charles', 'Erick', 'Mirna', 'Carlos', 'Luis', 'Mario'];
 function returnFirstTwoDrivers(drivers){
     return drivers.slice(0,2);
     ;}
 function returnLastTwoDrivers(drivers){   
-    return drivers.slice((drivers.length-2));
-    ;}
-let selectingDrivers = [(returnFirstTwoDrivers), (returnLastTwoDrivers)];
-//function createFareMultiplier(num){
-        //let multiplier = 5;
-        //const newFare = function () { return num * multiplier;}
-       // return newFare;
-   // ;
-//}
-//function fareDoubler(num){
-    //let multiplier = 2;
-   //return createFareMultiplier;
-    
-//}
+   return drivers.slice(-2);
+  ;}
+//create selectingDrivers array with functions as the elements
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
 
-//function fareTripler(num){
-   // let multiplier = 3;
-    //return createFareMultiplier;
-//}
+
+function createFareMultiplier(i){
+        return function(fare){return fare*i;}
+}
+const fareDoubler= createFareMultiplier(2);
+
+const fareTripler=createFareMultiplier(3);
+
 function selectDifferentDrivers(drivers, eitherfunction){
     //drivers is needed here as a parameter--if it is not, the array is out of scope
     //the inner functions will need access to the driver array and won't have it unless it's in the parent 
