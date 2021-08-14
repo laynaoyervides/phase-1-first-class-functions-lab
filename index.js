4d638d97749358802a7087b2f0155f38ef193188
Layna Oyervides <laynaoyervides@gmail.com>
2021-08-14 15:22:46 -0600

    Passing 4

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+
+const drivers = [Charles, Erick, Mirna, Carlos, Luis, Mario];
+function returnFirstTwoDrivers(driversArray){
+    return driversArray.slice[0,2];
+    ;}
+//function returnLastTwoDrivers(){;}
+//const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+function createFareMultiplier(num){
+        let multiplier = 5;
+        const newFare = function () { return num * multiplier;}
+        return newFare;
+    ;
+}
+createFareMultiplier(newFare);
+
+function fareDoubler(num){
+    let multiplier = 2;
+   return createFareMultiplier;
+    
+}
+
+function fareTripler(num){
+    let multiplier = 3;
+    return createFareMultiplier;
+}
+//function   selectDifferentDrivers(arrayOfDrivers, function) {}
+//function selectDifferentDrivers(arrayOfDrivers, function){}
